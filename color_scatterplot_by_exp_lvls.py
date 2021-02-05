32e1220c2707ab9abd794d9267e2451d6f2b8510
Ubuntu <zjing@umail.ucsb.edu>
2021-02-05 21:02:35 +0000

    Updated...

--- color_scatterplot_by_exp_lvls.py
+++ color_scatterplot_by_exp_lvls.py
@@ -95,7 +95,7 @@ def main():
             scaled_marker_size = scaled_marker_size*scaled_marker_size
             # s = 5000 / scatter_df.shape[0]
             plt.scatter(x=scatter_df["x"], y=scatter_df["y"], s=scaled_marker_size, c=values_df, cmap=cmaps[gene_index % len(cmaps)]) #Amp_3.mpl_colormap)
-            cbar = plt.colorbar(orientation='horizontal', aspect=40)
+            cbar = plt.colorbar(orientation='horizontal', pad=0.1, aspect=40)
             cbar.set_label(gene_id, rotation=0)
 
             plt.xlabel(dim_names[0])
