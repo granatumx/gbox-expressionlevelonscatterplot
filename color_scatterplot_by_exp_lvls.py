8638eb1367572a40a03b3db30c3190d96cf91269
David Garmire <david.garmire@gmail.com>
2023-02-13 15:56:04 -0500

    Updated...

--- color_scatterplot_by_exp_lvls.py
+++ color_scatterplot_by_exp_lvls.py
@@ -38,9 +38,7 @@ def invert_dict(my_map):
     return inv_map
 
 def show_percentages(gn, invdict, values, threshold):
-    percent_df = pd.concat([((values.loc[v, :] >= threshold).sum()+0.0)/len(v) for k, v in invdict.items()], axis=0)
-    #gn.add_result(" * Shape = {} * ".format(percent_df.shape), "markdown")
-    #gn.add_result(" * Values = {} * ".format(percent_df), "markdown")
+    percent_df = pd.concat([((values.loc[v, :] >= threshold).sum()+0.0).to_frame().T*100.0/len(v) for k, v in invdict.items()], axis=0).round(2)
     percent_df.index = list(invdict.keys())
     gn.add_pandas_df(percent_df.reset_index())
 
