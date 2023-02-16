63f1e6b429a923bd7de2bfe595b04db98025cb23
David Garmire <david.garmire@gmail.com>
2023-02-15 20:56:00 -0500

    Updated...

--- color_scatterplot_by_exp_lvls.py
+++ color_scatterplot_by_exp_lvls.py
@@ -37,16 +37,30 @@ def invert_dict(my_map):
         inv_map[v] = inv_map.get(v, []) + [k]
     return inv_map
 
-def show_percentages(gn, invdict, values, threshold):
-    percent_df = pd.concat([((values.loc[v, :] >= threshold).sum()+0.0).to_frame().T*100.0/len(v) for k, v in invdict.items()], axis=0).round(2)
-    percent_df.index = list(invdict.keys())
-    gn.add_pandas_df(percent_df.reset_index())
+
+def start_plot():
+    plt.clf()
+    fig, ax = plt.subplots()  
+    ax.xaxis.set_label_position("top")
+    ax.xaxis.tick_top()
+    divider = make_axes_locatable(ax)
+    ax.set_xlabel(dim_names[0])
+    ax.set_ylabel(dim_names[1])
+    ax.xaxis.set_tick_params(labeltop=False)
+    ax.xaxis.set_tick_params(labelbottom=False)
+    ax.yaxis.set_tick_params(labelleft=False)
+    ax.grid(False)
+    return ax, divider
+
+def add_annotations(ax, label_centers):
+    for grp, center in label_centers.items():
+        ax.annotation(grp, xy=center, xytext=center)
 
 def main():
     gn = Granatum()
 
     sample_coords = gn.get_import("viz_data")
-    df = gn.pandas_from_assay(gn.get_import("assay"))
+    df = gn.pandas_from_assay(gn.get_import("assay")).T
     labels = gn.get_import("labels")
     gene_ids = gn.get_arg("gene_ids")
     overlay_genes = gn.get_arg("overlay_genes")
@@ -61,6 +75,7 @@ def main():
     max_alpha = gn.get_arg("max_alpha")
     grey_level = gn.get_arg("grey_level")
     threshold = gn.get_arg("threshold")
+    label_groups = gn.get_arg("label_groups")
 
     coords = sample_coords.get("coords")
     dim_names = sample_coords.get("dimNames")
@@ -71,8 +86,9 @@ def main():
         label_inv = invert_dict(labels)
         label_inv = {k:list(set(v).intersection(set(df.columns))) for k, v in label_inv.items()}
 
+    # Set up colors
     cmaps = []
-    if overlay_genes and not merge_genes:         # Multiple colors required
+    if overlay_genes:         # Multiple colors required
         if max_colors == "":
             numcolors = len(gene_ids.split(','))
             cycol = cycle('bgrcmk')
@@ -80,123 +96,111 @@ def main():
                 cmaps = cmaps + [LinearSegmentedColormap("fire", produce_cdict(next(cycol), grey=grey_level, min_alpha=min_alpha, max_alpha=max_alpha), N=256)]
         else:
             for col in max_colors.split(','):
-                col = col.strip()
-                cmaps = cmaps + [LinearSegmentedColormap("fire", produce_cdict(col, grey=grey_level, min_alpha=min_alpha, max_alpha=max_alpha), N=256)]
+                cmaps = cmaps + [LinearSegmentedColormap("fire", produce_cdict(col.strip(), grey=grey_level, min_alpha=min_alpha, max_alpha=max_alpha), N=256)]
 
     else:
         if max_colors == "":
             cmaps = cmaps + [LinearSegmentedColormap("fire", cdict, N=256)]
         else:
             for col in max_colors.split(','):
-                col = col.strip()
-                cmaps = cmaps + [LinearSegmentedColormap("fire", produce_cdict(col, grey=grey_level, min_alpha=min_alpha, max_alpha=max_alpha), N=256)]
-
-    colorbar_height = 10
-    plot_height = 650
-    num_cbars = 1
-    if overlay_genes:
-        num_cbars = len(gene_ids.split(','))
-    cbar_height_ratio = plot_height/(num_cbars*colorbar_height)
-    fig, ax = plt.subplots() # plt.subplots(1+num_cbars, 1, gridspec_kw={'height_ratios': [cbar_height_ratio] + [1]*num_cbars})
-    ax.xaxis.set_label_position("top")
-    ax.xaxis.tick_top()
-    divider = make_axes_locatable(ax)
+                cmaps = cmaps + [LinearSegmentedColormap("fire", produce_cdict(col.strip(), grey=grey_level, min_alpha=min_alpha, max_alpha=max_alpha), N=256)]
 
     gene_index = -1
-    numgenes = len(gene_ids.split(','))
-    transposed_df = df.T
+    numgenes = len(gene_ids.split(','))    # Probably can drop this
 
-    merge_scatters = []  # will be the list of scatter_dfs to merge
+    # Split gene list first into gene names with corresponding scale
+    gene_scale_tuples = [(gene_eqs.trim(), 1.0) if len(gene_eqs.trim().split("="))>1 else tuple(gene_eqs.trim().split("=")) for gene_eqs in gene_ids.split(',')]
+    gene_list = [gene for gene, scale in gene_scale_tuples]
+    cmap_genes = {gene_list[i]:cmaps[i] for i in range(len(cmaps))}
 
-    # Only do merge_genes plot at the end
-    for gene_id in gene_ids.split(','):
-        gene_id = gene_id.strip()
-        gene_ids_equal = gene_id.split("=")
-        if len(gene_ids_equal) > 1:
-            scale = float(gene_ids_equal[1])
-        else:
-            scale = 1.0/numgenes 
-        gene_id = gene_ids_equal[0]
-        gene_index = gene_index + 1
-        if gene_id in df.index:
-            # Data preparation
-            if merge_genes and gene_index < numgenes - 1:
-                merge_scatters.append(df.loc[gene_id, :])
-                continue
-            else:
-                if merge_genes:
-                    values = pd.concat(merge_scatters, axis=1).sum(axis=1) * scale
-                else:
-                    values = transposed_df[gene_id] * scale
-                values = (values - values.mean()) / values.std(ddof=0) if convert_to_zscore else values
-                scatter_df = pd.DataFrame({"x": [a[0] for a in coords.values()], "y": [a[1] for a in coords.values()], "value": values}, index=coords.keys())
+    # if the gene ID entered is not present in the assay
+    # Communicate it to the user and output a table of available gene ID's
+    if len(set(gene_list).difference(set(df.columns))) > 0:
+        description = "The following gene(s) {} is/are not present in the assay. See the step that generated the assay".format(set(gene_list).difference(set(df.columns)))
+        genes_in_assay = pd.DataFrame(df.columns.tolist(), columns=['Gene unavailable in assay: choose from below'])
+        gn.add_pandas_df(genes_in_assay, description)
+        gn.commit()
+        break
+    
+    # Create scatter_df, extra column if merging, and show counts in a column if merging
+    merge_scatters = [df[gene]*scale for gene, scale in gene_scale_tuples]  # will be the list of scatter_dfs to merge
+    values = pd.concat(merge_scatters, axis=1)
+    if merge_genes:
+        values[gene_ids] = values.sum(axis=1)
+    if convert_to_zscore:
+        values = (values - values.mean()) / values.std(ddof=0)   # Will do all columns at once
+    if labels is not None:
+        percentages = pd.concat([((values.loc[v, :] >= threshold).sum()+0.0).to_frame().T*100.0/len(v) for k, v in label_inv.items()], axis=0).round(2)
+        percentages["# Cells"] = [len(v) for k, v in label_inv.items()]
+        percentages.index = list(invdict.keys())
+    else:
+        percentages = ((values >= threshold).sum()+0.0).to_frame().T*100.0 / values.shape[0]
+        percentages["# Cells"] = values.shape[0]
+        percentages.index = ["All (no labels provided)"]
+    gn.add_result("* Percentages of each group expressing at threshold {} *".format(threshold), "markdown")
+    gn.add_pandas_df(percentages.reset_index())
 
-            if not overlay_genes:     # New gene appears in separate plot
-                plt.clf()
-                fig, ax = plt.subplots()  # plt.subplots(1+num_cbars, 1, gridspec_kw={'height_ratios': [cbar_height_ratio] + [1]*num_cbars})
-                ax.xaxis.set_label_position("top")
-                ax.xaxis.tick_top()
-                divider = make_axes_locatable(ax)
+    # Export files of the data for other processing
+    gn.export(percentages.to_csv(), "percentage_summaries.csv", kind='raw', meta=None, raw=True)
+    if labels is not None:
+        for k, v in label_inv.items():
+            gn.export(values.loc[v, :].to_csv(), "cell_values_{}.csv".format(k), kind='raw', meta=None, raw=True)
+    else:
+        gn.export(values.to_csv(), "cell_values.csv", kind='raw', meta=None, raw=True)
 
-            values_df = np.clip(scatter_df["value"], min_level, max_level, out=None)
-            min_value = np.nanmin(values_df)
-            max_value = np.nanmax(values_df)
-            scaled_marker_size = (max_marker_area-min_marker_area)*(values_df-min_value)/(max_value-min_value) + min_marker_area
-            scaled_marker_size = scaled_marker_size*scaled_marker_size
-            # s = 5000 / scatter_df.shape[0]
-            if not (merge_genes and gene_index < numgenes - 1):
-                scatter = ax.scatter(x=scatter_df["x"], y=scatter_df["y"], s=scaled_marker_size, c=values_df, cmap=cmaps[gene_index % len(cmaps)]) #Amp_3.mpl_colormap)
+    x_df = pd.DataFrame({"x": [a[0] for a in coords.values()]}, index=coords.keys())
+    y_df = pd.DataFrame({"y": [a[1] for a in coords.values()]}, index=coords.keys())
 
-                if labels is not None and overlay_genes:
-                    msg = "* Percentages of each group expressing {} at threshold {} *".format(gene_id, threshold)
-                    gn.add_result(msg, "markdown")
-                    show_percentages(gn, label_inv, values_df.to_frame(), threshold)
-                cax = divider.append_axes('bottom', size=0.15, pad=0.01)
-                cbar = fig.colorbar(scatter, cax=cax, orientation='horizontal', aspect=300)
-                #cbar = fig.colorbar(scatter, cax=ax[1+(gene_index%num_cbars)], orientation='horizontal', aspect=40)
-                #cbar.set_label(gene_id, rotation=0)
+    # Get the centers of each cluster on the coordinates
+    if labels is not None:
+        label_centers = {k:(x_df.loc[v, "x"].mean(), y_df.loc[v, "y"]) for k, v in label_inv.items()}
 
-                ax.set_xlabel(dim_names[0])
-                ax.set_ylabel(dim_names[1])
+    values_df = np.clip(values, min_level, max_level, out=None)
+    values_df.loc[x_df.index, :]       # Ensure alignment
+    min_value = np.nanmin(values_df)
+    max_value = np.nanmax(values_df)
+    scaled_marker_size = (max_marker_area-min_marker_area)*(values_df-min_value)/(max_value-min_value) + min_marker_area
+    scaled_marker_size = scaled_marker_size*scaled_marker_size
 
-                if not merge_genes:
-                    cbar.ax.set_ylabel(gene_id, rotation=0)
-                    cax.yaxis.set_label_coords(0.08, 0.0)
-
-                ax.xaxis.set_tick_params(labeltop=False)
-                ax.xaxis.set_tick_params(labelbottom=False)
-                ax.yaxis.set_tick_params(labelleft=False)
-                ax.grid(False)
-
-            if merge_genes:
-                if gene_index == numgenes - 1:
-                    cax.tick_params(axis="x",direction="inout", pad=-1)
-                    gn.add_current_figure_to_results("Scatter-plot of {} expression".format(gene_ids), dpi=75)
-                    if labels is not None:
-                        msg = "* Percentages of each group expressing merge at threshold {} *".format(threshold)
-                        gn.add_result(msg, "markdown")
-                        show_percentages(gn, label_inv, values_df.to_frame(), threshold)
-            elif not overlay_genes:
-                cax.tick_params(axis="x",direction="inout", pad=-1)
-                gn.add_current_figure_to_results("Scatter-plot of {} expression".format(gene_id), dpi=75)
-                if labels is not None:
-                    msg = "* Percentages of each group expressing {} at threshold {} *".format(gene_id, threshold)
-                    gn.add_result(msg, "markdown")
-                    show_percentages(gn, label_inv, values_df.to_frame(), threshold)
-            else:
-                if gene_index < numgenes-1:
-                    cax.tick_params(axis="x",direction="in", pad=-1)
-                    cax.xaxis.set_ticklabels([])
-                else:
-                    cax.tick_params(axis="x",direction="inout", pad=-1)
-        else:
-            # if the gene ID entered is not present in the assay
-            # Communicate it to the user and output a table of available gene ID's
-            description = 'The selected gene is not present in the assay. See the step that generated the assay'
-            genes_in_assay = pd.DataFrame(df.index.tolist(), columns=['Gene unavailable in assay: choose from below'])
-            gn.add_pandas_df(genes_in_assay, description)
-    if overlay_genes:
-        gn.add_current_figure_to_results("Scatter-plot of {} expression".format(gene_ids), height=650+100*len(gene_ids.split(',')), dpi=75)
+    # Now we can easily generate the scatter plots
+    if merge_genes:
+        # Generate scatter with just merged genes
+        ax, divider = start_plot()
+        scatter = ax.scatter(x=x_df["x"], y=y_df["y"], s=scaled_marker_size[gene_ids], c=values_df[gene_ids], cmap=cmaps[len(cmaps)-1]) #Amp_3.mpl_colormap)
+        cax = divider.append_axes('bottom', size=0.15, pad=0.01)
+        cax.tick_params(axis="x",direction="inout", pad=-1)
+        cbar = fig.colorbar(scatter, cax=cax, orientation='horizontal', aspect=300)
+        if labels is not None and label_groups:
+            add_annotations(ax, label_centers)
+        gn.add_current_figure_to_results("Scatter-plot of summed {} expression".format(gene_ids), dpi=75)
+    else if overlay_genes:
+        ax, divider = start_plot()
+        cax = None # Will be the last color axis added
+        for gene in gene_list:
+            scatter = ax.scatter(x=x_df["x"], y=y_df["y"], s=scaled_marker_size[gene], c=values_df[gene], cmap=cmap_genes[gene])
+            cax = divider.append_axes('bottom', size=0.15, pad=0.01)
+            cbar = fig.colorbar(scatter, cax=cax, orientation='horizontal', aspect=300)
+            cbar.ax.set_ylabel(gene, rotation=0)
+            cax.yaxis.set_label_coords(0.08, 0.0)
+            cax.tick_params(axis="x",direction="in", pad=-1)
+            cax.xaxis.set_ticklabels([])
+        cax.tick_params(axis="x",direction="inout", pad=-1)
+        if labels is not None and label_groups:
+            add_annotations(ax, label_centers)
+        gn.add_current_figure_to_results("Scatter-plot of {} expression".format(gene_ids), dpi=75)
+    else:
+        for gene in gene_list:
+            ax, divider = start_plot()
+            scatter = ax.scatter(x=x_df["x"], y=y_df["y"], s=scaled_marker_size[gene], c=values_df[gene], cmap=cmap_genes[gene])
+            cax = divider.append_axes('bottom', size=0.15, pad=0.01)
+            cbar = fig.colorbar(scatter, cax=cax, orientation='horizontal', aspect=300)
+            cbar.ax.set_ylabel(gene, rotation=0)
+            cax.yaxis.set_label_coords(0.08, 0.0)
+            cax.tick_params(axis="x",direction="inout", pad=-1)
+            cax.xaxis.set_ticklabels([])
+            if labels is not None and label_groups:
+                add_annotations(ax, label_centers)
+            gn.add_current_figure_to_results("Scatter-plot of {} expression".format(gene), dpi=75)
 
     gn.commit()
 
